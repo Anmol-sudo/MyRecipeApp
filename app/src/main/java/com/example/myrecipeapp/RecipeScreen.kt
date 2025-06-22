3a04084af5b1158ed36bccfcec0b55fe780523a7
Anmol-Sudo <anmolshiv1974@gmail.com>
2025-06-22 14:57:55 +0530

    Updated RecipeScreen and CategoryItem to navigate to detail screen
    The `RecipeScreen` Composable now accepts a `navigateToDetail` lambda function, which is passed down to `CategoryScreen` and then to `CategoryItem`.
    
    The `CategoryItem` Composable has been made clickable, and when clicked, it invokes the `navigateToDetail` function with the corresponding `Category` object. This allows navigation to a detail screen when a category item is clicked.

--- app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -23,7 +24,9 @@ import androidx.compose.ui.text.font.FontWeight
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier){
+fun RecipeScreen(modifier: Modifier = Modifier,
+                 navigateToDetail: (Category) -> Unit
+                 ){
     val recipeViewModel: MainViewModel = viewModel()
     val viewstate by recipeViewModel.categoriesState
     Box(modifier = Modifier.fillMaxSize()){
@@ -37,25 +40,33 @@ fun RecipeScreen(modifier: Modifier = Modifier){
             }
             else ->{
                 // Display Categories
-                CategoryScreen(categories = viewstate.list)
+                CategoryScreen(categories = viewstate.list,
+                    navigateToDetail)
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>){
+fun CategoryScreen(categories: List<Category>,
+                   navigateToDetail: (Category) -> Unit
+                   ){
     LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
         items(categories) {
             category ->
-            CategoryItem(category = category)
+            CategoryItem(category = category, navigateToDetail)
         }
     }
 }
 
 @Composable
-fun CategoryItem(category: Category){
-    Column(modifier = Modifier.padding(8.dp).fillMaxSize(),
+fun CategoryItem(category: Category,
+                 navigateToDetail: (Category) -> Unit
+                 ){
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxSize().clickable{ navigateToDetail(category) },
+
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
